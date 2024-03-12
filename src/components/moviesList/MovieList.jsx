8e4e50c9617747ef118cc52f9c1df54263d84ff4
NovichokUA <zubr7333@gmail.com>
2024-03-12 19:11:47 +0200

    .

---
+++ src/components/moviesList/MovieList.jsx
@@ -0,0 +1,28 @@
+import { nanoid } from "nanoid";
+import { Link, useLocation } from "react-router-dom";
+import MovieCard from "./MovieCard";
+import css from "./MovieList.module.css";
+
+function MovieList({ movies }) {
+  const location = useLocation();
+
+  return (
+    <div>
+      <ul className={css.container}>
+        {movies &&
+          movies.map((movie) => {
+            let id = nanoid();
+            return (
+              <li key={id}>
+                <Link to={`/movies/${movie.id}`} state={location}>
+                  {<MovieCard movie={movie} />}
+                </Link>
+              </li>
+            );
+          })}
+      </ul>
+    </div>
+  );
+}
+
+export default MovieList;
