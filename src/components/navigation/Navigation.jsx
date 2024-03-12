1909fa5dfc7ba7249ea5efca05ae8b2e19f8b7c3
NovichokUA <zubr7333@gmail.com>
2024-03-12 21:34:36 +0200

    add css

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,10 +1,26 @@
+import clsx from "clsx";
 import { NavLink } from "react-router-dom";
+import css from "./Navigation.module.css";
 
 function Navigation() {
   return (
     <nav>
-      <NavLink to="/">Home Page</NavLink>
-      <NavLink to="/movies">Movies</NavLink>
+      <NavLink
+        to="/"
+        className={({ isActive }) => {
+          return clsx(css.link, isActive && css.isActive);
+        }}
+      >
+        Home Page
+      </NavLink>
+      <NavLink
+        to="/movies"
+        className={({ isActive }) => {
+          return clsx(css.link, isActive && css.isActive);
+        }}
+      >
+        Movies
+      </NavLink>
     </nav>
   );
 }
