10deff8c129ffbeae2a88baff5cd3db341dea650
NovichokUA <zubr7333@gmail.com>
2024-03-13 13:04:41 +0200

    add css

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import css from "./NotFoundPage.module.css";
 
 export default function NotFoundPage() {
   return (
     <>
-      <Link to="/">Home</Link>
-      <p>Anything Is Not Found</p>
+      <button className={css.btn}>
+        <Link to="/">Go Home</Link>
+      </button>
+
+      <p className={css.p}>Anything Is Not Found</p>
     </>
   );
 }
